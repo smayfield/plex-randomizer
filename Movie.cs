9238658c71eeff5018333e2c65a1d156b9f2a834
Scott Mayfield <mayfield@magicworld.com>
2025-04-27 20:07:53 -0400

    cleanup

---
+++ Movie.cs
@@ -0,0 +1,10 @@
+namespace Plex;
+
+class Movie
+{
+    public string Title { get; set; }
+    public int Year { get; set; }
+    public string Summary { get; set; }
+    public double Rating { get; set; }
+    public long Duration { get; set; }
+}
